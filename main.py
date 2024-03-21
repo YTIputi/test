83bf74791d4c8b77b8c7d78c9beb2b208c36ce35
Gleb Ovcharov <glebov274@gmail.com>
2024-03-21 13:24:19 +0400

    click

--- main.py
+++ main.py
@@ -1,1 +1,51 @@
-print(1)
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
+from PyQt6.QtCore import Qt
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('My App')
+
+        self.label = QLabel('Click in this window')
+        self.label1 = QLabel('Here is text:')
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.label1)
+        layout.addWidget(self.label)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
+    def mousePressEvent(self, e):
+        if e.button() == Qt.MouseButton.LeftButton:
+                self.label.setText('mousePressEvent LEFT')
+        elif e.button == Qt.MouseButton.MiddleButton:
+            self.label.setText('mousePressEvent MIDDLE')
+        elif e.button() == Qt.MouseButton.RightButton:
+            self.label.setText('mousePressEvent RIGHT')
+
+    def mouseReleaseEvent(self, e):
+        if e.button() == Qt.MouseButton.LeftButton:
+            self.label.setText('mouseReleaseEvent LEFT')
+        elif e.button() == Qt.MouseButton.MiddleButton:
+            self.label.setText('mouseReleaseEvent MIDDLE')
+        elif e.button() == Qt.MouseButton.RightButton:
+            self.label.setText('mouseReleaseEvent RIGHT')
+
+    def mouseDoubleClickEvent(self, e):
+        if e.button() == Qt.MouseButton.LeftButton:
+            self.label.setText('mouseDoubleClickEvent LEFT')
+        elif e.button() == Qt.MouseButton.MiddleButton:
+            self.label.setText('mouseDoubleClickEvent MIDDLE')
+        elif e.button() == Qt.MouseButton.RightButton:
+            self.label.setText('mouseDoubleClickEvent RIGHT')
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
